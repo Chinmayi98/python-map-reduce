69646c2ff02d04740187cac89250b717c6d8c185
Chinmayi <ambatichinmayi@gmail.com>
2020-10-07 12:45:19 -0500

    Added code to change output file names

--- 13reducer.py
+++ 13reducer.py
@@ -2,8 +2,8 @@
 # Easy to test
 # Not quite Hadoop-ready
 
-with open("output12sorted.txt","r") as sorted:
-  with open("output13reduced.txt", "w") as output:
+with open("12sorteroutput.txt","r") as sorted:
+  with open("13reduceroutput.txt", "w") as output:
 
     thisKey = ""
     thisValue = 0.0
